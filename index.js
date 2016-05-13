2ee63c6ba6e587b49df0cc70ae7719387159533c
Emma Luk <emma_luk1@hotmail.com>
2016-05-13 01:05:33 +0100

    emma-luk-assignment5
    1. Food Truck list with undefined data.
    2. Add Food Trucks
    3. remove Food Trucks

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your express server here
+//Emma Luk
+//Assignment-5
+//npm install
+//npm install -g nodemon    // not working 
+//cd emma\cv\study\washington.edu\JavaScript3\Module 5\assignments-5
+//node index
+//  http://127.0.0.1:3000/
+//ref: REST_APIs_pt2.pdf
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var trucksRoutes = require('./routes/truckRoutes');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// the bookRoutes is now a middle-ware callback
+app.use('/trucks', trucksRoutes);
+
+app.listen(3000, function () {
+    console.log('listening on port 3000');
+})
